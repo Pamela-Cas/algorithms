87db990fcc8785926b12bace4af9e394ed3d8b9d
Pamela-Cas <pxcx611@gmail.com>
2023-10-31 15:47:27 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
 
-# vqv!
+    students_count = 0
+
+    for period in permanence_period:
+        if not (isinstance(period[0], int)
+                and isinstance(period[1], int)):
+            return None
+        if target_time >= period[0] and target_time <= period[1]:
+            students_count += 1
+    return students_count
